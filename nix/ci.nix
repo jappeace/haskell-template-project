b784af541b967fd120afaa465ad1ca181e54e6de
Jappie Klooster <jappieklooster@hotmail.com>
2020-10-13 11:20:12 +0200

    Reduce the amount of supported compilers

--- nix/ci.nix
+++ nix/ci.nix
@@ -2,9 +2,9 @@
     pkgs ? import ./pin.nix {}, ...
 }:
 {
+  # https://github.com/NixOS/nixpkgs/blob/master/pkgs/top-level/haskell-packages.nix#L47
   ghc-def = import ../default.nix { inherit pkgs; };
-  ghc-865 = import ../default.nix {compiler = "ghc865"; inherit pkgs; };
-  ghc-883 = import ../default.nix {compiler = "ghc883"; inherit pkgs;};
-  ghc-810 = import ../default.nix {compiler = "ghc8101"; inherit pkgs;};
+  ghc-810 = import ../default.nix {compiler = "ghc8102"; inherit pkgs;};
+  # ghc-901 = import ../default.nix {compiler = "ghc901"; inherit pkgs;};
   # test = pkgs.nixosTest nix/test.nix;
 }
--- nix/pin.nix
+++ nix/pin.nix
@@ -2,5 +2,5 @@ import (builtins.fetchGit {
     # Descriptive name to make the store path easier to identify
     name = "nixos-pin-19.08.2020";
     url = https://github.com/nixos/nixpkgs/;
-    rev = "f7651718b5966be91c4d9a6bbcbd468bd7d17bc8";
+    rev = "6baf442a9e1336d46668fbb4564fa9dc9b5ae1c5";
     })
