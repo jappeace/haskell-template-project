686761be2cbe0d5b37baedabc2cdbc812ce1e85a
Jappie Klooster <jappieklooster@hotmail.com>
2022-01-08 23:36:27 +0100

    Upgrade pin

--- nix/pin.nix
+++ nix/pin.nix
@@ -1,6 +1,6 @@
 import (builtins.fetchGit {
     # Descriptive name to make the store path easier to identify
-    name = "nixos-pin-2021.10.13";
+    name = "nixos-pin-2022.01.08";
     url = "https://github.com/nixos/nixpkgs/";
-    rev = "9a0c36e8d4fdaa1d236cf8de5b377f8ccc28436a";
+    rev = "d53d2147eff16f01b2345d4afed2de362979f551";
     })
