3efc055e2f3653f2abc6406946f0dae21d14e1fe
Jappie Klooster <jappieklooster@hotmail.com>
2023-02-18 22:24:06 +0100

    add legacy support, use siginign key
    use signing key
    
    add legacy support
    
    add pre flake support
    
    update lock file

--- flake.nix
+++ flake.nix
@@ -5,9 +5,13 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
   };
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, flake-compat }:
     let
       pkgs = nixpkgs.legacyPackages.x86_64-linux;
       hpkgs = pkgs.haskell.packages.ghc943.override {
