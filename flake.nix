a47d4c737f064d595156f42a42eb69494ee78ea3
Jappie Klooster <jappieklooster@hotmail.com>
2023-02-18 17:59:55 +0100

    rip out the iohk bullshit

--- flake.nix
+++ flake.nix
@@ -1,55 +1,23 @@
 # https://github.com/input-output-hk/haskell.nix/blob/master/docs/tutorials/getting-started-flakes.md
 {
-  description = "Haskell template project";
+  description = "My Haskell project";
+
   inputs = {
-    haskellNix.url = "github:input-output-hk/haskell.nix";
-    nixpkgs.follows = "haskellNix/nixpkgs-2205";
-    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
   };
-  outputs = { self, nixpkgs, flake-utils, haskellNix }:
-    flake-utils.lib.eachSystem [
-      "x86_64-linux"
-      "x86_64-darwin"
-    ] (system:
+
+  outputs = { self, nixpkgs }:
     let
-      overlays = [
-        (haskellNix.overlay
-          (
-
-        builtins.trace "over"
-            (final: prev: {
-          # This overlay adds our project to pkgs
-          template = builtins.trace "templateeee"
-            final.haskell-nix.project' {
-              src = ./.;
-              compiler-nix-name = "ghc924";
-              # This is used by `nix develop .` to open a shell for use with
-              # `cabal`, `hlint` and `haskell-language-server`
-              shell.tools = {
-                cabal = {};
-                hlint = {};
-                hasktags  = {};
-                ghcid = {};
-                # haskell-language-server = {};
-              };
-              # Non-Haskell shell tools go here
-              shell.buildInputs = [
-              ];
-            };
-            })))
-      ];
-      # TODO this import is wrong.
-      # https://discourse.nixos.org/t/using-nixpkgs-legacypackages-system-vs-import/17462/3
-      pkgs = builtins.foldl'
-        (acc: overlay:
-          acc.lib.extends overlay)
-              nixpkgs.legacyPackages.${system}
-              overlays;
-      flake = pkgs.template.flake {};
-
-    in pkgs.lib.recursiveUpdate flake {
-      # Built by `nix build .`
-      # "${system}".default = flake.packages."template:exe:exe";
-      defaultPackage = flake.packages."template:exe:exe";
-    });
+      # `haskellPackages` contains all Haskell packages provided by Nixpkgs
+      # for the current system and GHC version.
+      pkgs = nixpkgs.legacyPackages.x86_64-linux.haskell.packages.ghc943.override {
+        overrides = self: super: {
+          # Add any project-specific dependencies here
+          # myDep = nixpkgs.pkgs.myDep;
+        };
+      };
+    in
+    {
+      defaultPackage.x86_64-linux =  pkgs.callCabal2nix "my-project" ./. { };
+    };
 }
