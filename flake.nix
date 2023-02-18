da82335d81841b64b19c85a94880affd837e9543
Jappie Klooster <jappieklooster@hotmail.com>
2023-02-18 22:31:02 +0100

    add pre flake support

--- flake.nix
+++ flake.nix
@@ -4,6 +4,10 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
   };
 
   outputs = { self, nixpkgs }:
