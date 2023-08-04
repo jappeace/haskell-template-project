265e610bf92229dc030e8ded4f3f5818397352be
Jappie Klooster <jappieklooster@hotmail.com>
2023-08-04 18:16:10 -0400

    wasm example

--- flake.nix
+++ flake.nix
@@ -9,9 +9,11 @@
       url = "github:edolstra/flake-compat";
       flake = false;
     };
+
+    wasm.url = "tarball+https://gitlab.haskell.org/ghc/ghc-wasm-meta/-/archive/master/ghc-wasm-meta-master.tar.gz";
   };
 
-  outputs = { self, nixpkgs, flake-compat }:
+  outputs = { self, nixpkgs, flake-compat, wasm }:
     let
       pkgs = nixpkgs.legacyPackages.x86_64-linux;
       hpkgs = pkgs.haskellPackages.override {
@@ -23,14 +25,13 @@
     {
       defaultPackage.x86_64-linux =  hpkgs.template-project;
       inherit pkgs;
+      inherit wasm;
       devShell.x86_64-linux = hpkgs.shellFor {
         packages = ps : [ ps."template-project" ];
         withHoogle = true;
 
         buildInputs = [
-          hpkgs.haskell-language-server
-          pkgs.ghcid
-          pkgs.cabal-install
+          wasm.packages.x86_64-linux.cabal
         ];
       };
     };
