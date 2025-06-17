cd40d2b4b0c82dfa49a24528c853a40d7a412ccc
Jappie Klooster <jappieklooster@hotmail.com>
2025-06-17 16:39:07 +0200

    async exception waeving

--- src/Template.hs
+++ src/Template.hs
@@ -5,3 +5,36 @@ where
 
 main :: IO ()
 main = putStrLn "hello, world flaky"
+
+-- make sure NOT to use safe exceptions (they'll neglect async)
+import Control.Exception(try, Exception)
+
+import Control.Concurrent.MVar
+import Control.Monad.Reader
+
+data MyIO a = MyIO { unMy :: ReaderT (MVar ()) IO a }
+   deriving (Functor)
+
+instance Applicative MyIO where
+  (<*>) fab fa = do
+     ab <- fab
+     a <- fa
+     pure (ab a)
+  pure a = MyIO { unMy = pure a }
+
+data TakeABreak = TakeABreak
+   deriving (Eq, Show)
+
+instance Exception TakeABreak
+instance Monad MyIO where
+   (>>=) (MyIO ma) aMb = MyIO {
+       unMy = do
+          breakyTime <- ask
+          eA <- liftIO $ try (runReaderT ma breakyTime)
+          case eA of
+             Left TakeABreak -> do
+                breakyTime <- ask
+                liftIO $ takeMVar breakyTime
+                a <- ma
+                unMy $ aMb a
+             Right a -> unMy $ aMb a }
