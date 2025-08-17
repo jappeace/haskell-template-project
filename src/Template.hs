6cbc1c60896115b0d2505fa2e8cd135ceb432fb8
Jappie Klooster <jappieklooster@hotmail.com>
2025-08-17 15:16:41 -0400

    when the walls felll

--- src/Template.hs
+++ src/Template.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE AllowAmbiguousTypes #-}
 
 module Template
   ( main
@@ -56,11 +57,14 @@ main = do
   putStrLn "generic magic"
   print (from secondPerson)
   putStrLn "disection"
-  print $ disectPerson $ (from secondPerson)
+  print $ disectPerson @Person $ (from secondPerson)
 
-disectPerson :: Rep Person () -> [Text]
+-- this won't work because it leaks out the generic represnetation to
+-- the callsite, so it's not generic in fact.
+disectPerson :: forall a name a0 b0 c0 d0 . (KnownSymbol name, Rep a ~  M1 D (MetaData name a0 b0 c0) d0) => Rep a () -> [Text]
 disectPerson
-  x = [getDataTypeConstructorName x]
+  x =
+  [getDataTypeConstructorName x]
 
 
 getDataTypeConstructorName :: forall name a b c d e . (KnownSymbol name) => D1 ('MetaData name a b c) d e -> Text
