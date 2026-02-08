7a8358bd692597b818077782019d352e2c213a24
Jappie Klooster <jappieklooster@hotmail.com>
2026-02-08 10:43:13 -0400

    Figure out the constrainy part

--- src/Template.hs
+++ src/Template.hs
@@ -10,7 +10,7 @@ module Template
 where
 
 import Prelude (fromInteger, Int, Char, Functor)
-import GHC.TypeLits (type (+), Nat)
+import GHC.TypeLits (type (+), type (<=), Nat)
 
 newtype MyInstruct (i :: Nat) a = MyInstruct a
   deriving (Functor)
@@ -19,11 +19,11 @@ newtype MyInstruct (i :: Nat) a = MyInstruct a
 pure :: a -> MyInstruct i a
 pure a = MyInstruct a
 
-(>>=) :: MyInstruct i a -> (a -> MyInstruct j b) -> MyInstruct (i + j) b
+(>>=) :: (i + j) <= 10 => MyInstruct i a -> (a -> MyInstruct j b) -> MyInstruct (i + j) b
 (>>=) (MyInstruct a) fab = case fab a of
   MyInstruct res -> MyInstruct res
 
-(>>) :: MyInstruct i a -> MyInstruct j b -> MyInstruct (i + j) b
+(>>) :: (i + j) <= 10 =>  MyInstruct i a -> MyInstruct j b -> MyInstruct (i + j) b
 (>>) a b = a >>= \_ -> b
 
 mov :: Char -> MyInstruct 1 ()
@@ -33,7 +33,7 @@ render :: Int -> MyInstruct 2 ()
 render _ = pure ()
 
 
-myTenProgram :: MyInstruct 10 ()
+myTenProgram :: MyInstruct 11 ()
 myTenProgram = do
   mov 'x'
   render 3
@@ -41,6 +41,7 @@ myTenProgram = do
   render 3
   mov 'x'
   render 3
+  mov 'x'
   mov 'x'
 
 myFiveProgram :: MyInstruct 5 ()
@@ -51,14 +52,3 @@ myFiveProgram = do
   mov 'x'
 
 
--- I think this is also possible, but it doesn't want to accept
--- a polymorphic a when it can calulate the number...
---
--- so normally you'd hide it, but then you can't compare
---
--- myBoundProgram :: forall a .  ((a <= 10)) =>  MyInstruct a ()
--- myBoundProgram = do
---   mov 'x'
---   mov 'x'
---   render 3
---   mov 'x'
